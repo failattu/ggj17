86b314b55cea7ba5d81f1cb08035d6456960c13c
Kalle Sirkesalo <kalle.sirkesalo@eficode.com>
2017-01-21 04:36:28 -0500

    project prep

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ go install github.com/user
 sudo apt-get install libopenal-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev xorg-dev libgl1-mesa-dev
 
 go get -u engo.io/engo
+
+
+TO COMPILE:
+
+go install github.com/failattu/ggj17
---
+++ src/github.com/failattu/ggj17/traffic.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"engo.io/engo"
+	"engo.io/ecs"
+)
+
+type myScene struct {}
+
+// Type uniquely defines your game type
+func (*myScene) Type() string { return "myGame" }
+
+// Preload is called before loading any assets from the disk,
+// to allow you to register / queue them
+func (*myScene) Preload() {}
+
+// Setup is called before the main loop starts. It allows you
+// to add entities and systems to your Scene.
+func (*myScene) Setup(*ecs.World) {}
+
+func main() {
+	opts := engo.RunOptions{
+		Title: "Hello World",
+		Width:  400,
+		Height: 400,
+	}
+	engo.Run(opts, &myScene{})
+}
